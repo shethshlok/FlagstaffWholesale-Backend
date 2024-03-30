ce649d14d069e0f02b74249367cbd8919666309d
root <root@srv496366.hstgr.cloud>
2024-03-30 02:29:44 +0000

    Added search for variant

--- medusa-config.js
+++ medusa-config.js
@@ -69,6 +69,9 @@ const plugins = [
               "handle",
               "thumbnail",
               "images",
+              "variants",
+              "variant_sku",
+              "options",
             ],
           },
           transformer: (product) => ({ 
